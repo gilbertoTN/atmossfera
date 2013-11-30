4d67310743e5764ff3cfd314f9ae4e1939450c48
gtovar1985 <gtovar@incaztech.com>
2013-11-30 11:26:25 -0600

    Rails empty application, gem file have ruby 2.0 and 12_factor, database config and gitignore config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Atmossfera::Application.config.secret_key_base = '7a5946107dddda468b774d0580e7dea71c2f2eec634c8883d18cadc32834bbe3c97eeea446f93896de2dc91f88d075239c5114357943a87d2fcee955d8a43e68'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Atmossfera::Application.config.session_store :cookie_store, key: '_atmossfera_session'
